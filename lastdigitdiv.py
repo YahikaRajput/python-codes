243f83ae0b70671c6bea853b6f158cb84780ef83
Yashika Rajput <rajputyashika414@gmail.com>
2026-03-31 14:00:05 +0530

    Delete lastdigitdiv.py

--- lastdigitdiv.py
+++
@@ -1,7 +0,0 @@
-num = int(input("Enter a number: "))
-b = num % 10
-
-if b % 5 == 0:
-    print("The last digit is divisible by 5", b)
-else:
-    print("The last digit is not divisible by 5", b)    
