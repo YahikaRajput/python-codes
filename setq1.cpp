26df4d3e2f184982c1baceba0c40575bc62bf97c
Yashika Rajput <rajputyashika414@gmail.com>
2026-03-31 14:07:49 +0530

    Delete setq1.cpp

--- setq1.cpp
+++
@@ -1,4 +0,0 @@
-// Q1 write menu driven program to impliment all bult- in function of a list.
-// Q2 write a menu driven program to imlpliment all bulti - in functions of tuple.
-// Q3 write menu driven program to impliment all bulti - in funtions of dictionary.
-// Q4 write a menu driven program to impliment all bulti - in functions of string.
